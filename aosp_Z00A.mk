e1a4b27085c28903d44f0a939f7b95033b2b77dd
Sammy854 <sammybiella@gmail.com>
2016-07-08 12:35:45 +0200

    Initial AOSP RRO setup
    Change-Id: Ifdf48edf9e4f231aefa2850894a1ebcbeff5be1f

--- aosp_Z00A.mk
+++ aosp_Z00A.mk
@@ -2,20 +2,20 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common OMNI stuff.
-$(call inherit-product, vendor/omni/config/gsm.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product-if-exists, vendor/asus/Z00A/Z00A-vendor.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_Z00A
+PRODUCT_NAME := aosp_Z00A
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z00A
 PRODUCT_MANUFACTURER := asus
